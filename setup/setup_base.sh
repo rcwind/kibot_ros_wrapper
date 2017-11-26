03ae516d9ff2f4b57a8ea3a3293496a4049400e8
zouwubing <1756681646@qq.com>
2017-11-26 17:54:15 +0800

    changed serial to kobuki in udev file, discard ttyUSB0

--- setup/setup_base.sh
+++ setup/setup_base.sh
@@ -1,12 +1,13 @@
 #!/bin/sh 
+# 该脚本已经废弃
 # usage: source setup_base.sh
 # turtlebot默认的base就是kobuki, 默认的3d sensor是 asus xtion pro
-#export TURTLEBOT_BASE=kobuki
-#export TURTLEBOT_STACKS=hexagons
-#export TURTLEBOT_3D_SENSOR=kinect
-#export TURTLEBOT_SIMULATION=false
+# export TURTLEBOT_BASE=kobuki
+# export TURTLEBOT_STACKS=hexagons
+# export TURTLEBOT_3D_SENSOR=kinect
+# export TURTLEBOT_SIMULATION=false
 # 修改成底盘控制器的串口号
-serial=ttyUSB0
-#serial=rfcomm0
+# serial=ttyUSB0
+# serial=rfcomm0
 
-export TURTLEBOT_SERIAL_PORT=/dev/$serial    
+# export TURTLEBOT_SERIAL_PORT=/dev/$serial    

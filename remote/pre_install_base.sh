f8a4a27dfff791c21984ebc8a7d58f64cb9180a0
zouwubing <251349552@qq.com>
2017-12-23 14:36:24 +0800

    add install base

---
+++ remote/pre_install_base.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sudo apt-get install \
+        tmux \
+        expect \
+        ssh \
+        sshfs \
+        ros-indigo-kobuki \
+        ros-indigo-kobuki-core \
+        ros-indigo-turtlebot \
+

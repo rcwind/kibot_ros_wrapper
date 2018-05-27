59c00c0e024314d733720d580e2513a548740a60
zouwubing <1756681646@qq.com>
2018-05-27 17:36:47 +0800

    fixed: keyboard teleop

--- remote/env.sh
+++ remote/env.sh
@@ -6,12 +6,12 @@
 NET_DEV=wlan0
 export IP_OF_REMOTE=`ifconfig $NET_DEV |awk -F\: '/inet / {print $2}'|awk '{print $1}'`
 # 用户配置这个，kibot机器人上的笔记本ip地址
-export IP_OF_KIBOT=192.168.2.191
+export IP_OF_KIBOT=172.29.0.3
 #export IP_OF_kibot=192.168.10.213
 # 用户配置这个，kibot机器人上笔记本ssh登录名字
-export USERNAME_IN_KIBOT=zouwubing
+export USERNAME_IN_KIBOT=kidil
 # 用户配置这个，kibot机器人上笔记本ssh登录后提示的信息，也即终端的输出前缀，比如[kibot@localhost ~]，可以使用通配符
-export LOGIN_PROMPT="zouwubing@host:*"
+export LOGIN_PROMPT="kidil@orangepipcplus:*"
 #export LOGIN_PROMPT="*~$*"
 
 export ROS_MASTER_URI=http://$IP_OF_KIBOT:11311
--- remote/teleop_kibot_use_keyboard.sh
+++ remote/teleop_kibot_use_keyboard.sh
@@ -4,13 +4,12 @@ this_script_dir=$(cd "$(dirname "$0")"; pwd)
 file_prefix=bringup_kibot
 source $this_script_dir/env.sh
 #make the minimal software been launched
-#ssh -t $USERNAME_IN_kibot@$IP_OF_kibot "sh /home/$USERNAME_IN_kibot/kibot_ros_wrapper/kibot/kill_tmux_window.sh"
-#launch_cmd="$this_script_dir/expect/$file_prefix.expect $IP_OF_kibot $USERNAME_IN_kibot $LOGIN_PROMPT"
-#launch_cmd="$this_script_dir/kibot.expect $IP_OF_kibot $USERNAME_IN_kibot $LOGIN_PROMPT $file_prefix 1"
+ssh -t $USERNAME_IN_KIBOT@$IP_OF_KIBOT "sh /home/$USERNAME_IN_KIBOT/kibot_ros_wrapper/kibot/kill_tmux_window.sh"
+launch_cmd="$this_script_dir/kibot.expect $IP_OF_KIBOT $USERNAME_IN_KIBOT $LOGIN_PROMPT $file_prefix 1"
 sleep 1
-#tmux kill-window -t :0 >>/dev/null || true
-#tmux new-window -k -t :0 "$launch_cmd"
-#tmux select-window -t :0
+tmux kill-window -t :0 >>/dev/null || true
+tmux new-window -k -t :0 "$launch_cmd"
+tmux select-window -t :0
 
 #start rviz already configured to visualize the robot and its sensor's output
 sleep 5
